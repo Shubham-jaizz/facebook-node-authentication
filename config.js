48dfccec3ad45787b7385d3e130317ead5a05094
shubham jaiswal <simashubham.mj@gmail.com>
2022-06-21 11:01:44 +0530

    save changes

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '762412698446918', 
+		'clientSecret':  '93be6453fa0b1eb1ed6217c9a666fa97',
+		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
+	}
+}
